9e82e59943636007b92327a92c8242e18310ce5f
sarthu210 <sarthu102@gmail.com>
2024-08-17 11:36:53 +0530

    Completed My First Package To Comapre Two Strings

---
+++ index.js
@@ -0,0 +1,13 @@
+function compare(string1 ,string2)
+{
+    if(string1 === string2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+module.exports = compare;
